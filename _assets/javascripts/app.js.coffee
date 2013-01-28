2cba826df92ccee4286fd002fc668f6fafc9e863
Eumir Gaspar <eumir@aelogica.com>
2013-01-28 22:50:25 +0800

    Added jekyll assets + compass

---
+++ _assets/javascripts/app.js.coffee
@@ -0,0 +1,4 @@
+#= require vendor/jquery
+#
+$ ->
+  alert "wahoo!"
--- config.rb
+++ config.rb
@@ -2,10 +2,7 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "stylesheets/sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+sass_dir = "_assets/stylesheets"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
